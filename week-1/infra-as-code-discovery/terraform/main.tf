b78989649b620587934ded68edb653a1e6983b58
Fabien Huitelec <fabien@huitelec.fr>
2024-09-22 23:30:57 +0200

    feat(week-1): complete main.tf with resource group

--- week-1/infra-as-code-discovery/terraform/main.tf
+++ week-1/infra-as-code-discovery/terraform/main.tf
@@ -2,11 +2,20 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> X.X.X"
+      version = "~> 4.3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = ""
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "test"
+  location = "francecentral"
+  tags = {
+    "hello" = "world"
+  }
 }
