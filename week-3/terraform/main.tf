c82d1fc94947792b7981f246d600f34d03cf7027
Fabien Huitelec <fabien@huitelec.fr>
2024-10-05 22:30:34 +0200

    refactor(week-3): introduce locals

--- week-3/terraform/main.tf
+++ week-3/terraform/main.tf
@@ -10,16 +10,23 @@ resource "azurerm_resource_group" "week_3" {
   location = var.location
 }
 
+locals {
+  resource_group = azurerm_resource_group.week_3.name
+  location = azurerm_resource_group.week_3.location
+}
+
 module "database" {
   source  = "./modules/database"
   count = 0
 
-  resource_group_name = azurerm_resource_group.week_3.name
-  location = azurerm_resource_group.week_3.location
+  resource_group_name = local.resource_group
+  location = local.location
+
   entra_administrator_tenant_id = data.azurerm_subscription.current.tenant_id
   entra_administrator_object_id = data.azuread_user.user.object_id
   entra_administrator_principal_type = "User"
   entra_administrator_principal_name = data.azuread_user.user.user_principal_name
+
   server_name = var.server_name != null ? var.server_name : format("playground-computing-%s", random_string.server_name_suffix.result)
   database_administrator_login = var.database_username
   database_administrator_password = var.database_password
