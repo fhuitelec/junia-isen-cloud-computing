59ca73dbc45a51fcc6cd7553592db982029637d6
Albane <149374832+albanesflt@users.noreply.github.com>
2024-10-07 07:31:22 +0000

    ajout des fichiers à la branche

--- week-1/infra-as-code-discovery/terraform/main.tf
+++ week-1/infra-as-code-discovery/terraform/main.tf
@@ -2,11 +2,20 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> X.X.X"
+      version = "~> 4.3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = "cb19b88f-dac3-4425-a42b-f1ba6dbc9fc1"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "lol"
+  location = "francecentral"
+  tags = {
+    hello = "world"
+  }
 }
